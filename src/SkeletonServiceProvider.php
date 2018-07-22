041cc96c64fefbbc6624069f1df6000a601817bb
I'm Liam <liamhammett@gmail.com>
2018-07-22 01:27:50 +0100

    ✨ Add more Laravel package placeholders

--- src/SkeletonServiceProvider.php
+++ src/SkeletonServiceProvider.php
@@ -13,16 +13,20 @@ class SkeletonServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/skeleton.php' => config_path('skeleton.php'),
+                __DIR__.'/../config/config.php' => config_path('skeleton.php'),
             ], 'config');
 
-            /*
-            $this->loadViewsFrom(__DIR__.'/../resources/views', 'skeleton');
+            if (! class_exists('CreateSkeletonTable')) {
+                $this->publishes([
+                    __DIR__.'/../database/migrations/create_skeleton_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_skeleton_table.php'),
+                ], 'migrations');
+            }
 
             $this->publishes([
-                __DIR__.'/../resources/views' => base_path('resources/views/vendor/skeleton'),
+                __DIR__.'/../resources/views' => resource_path('views/vendor/skeleton'),
             ], 'views');
-            */
+
+            $this->loadViewsFrom(__DIR__.'/../resources/views', 'skeleton');
         }
     }
 
@@ -32,5 +36,15 @@ class SkeletonServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'skeleton');
+
+        // $this->app->singleton(SingletonClass::class, function () {
+        //     $className = config('skeleton.class_name');
+        //     return new SingletonClass(new $className);
+        // });
+
+        // $this->app->bind('command.skeleton:command-name', Command::class);
+        // $this->commands([
+        //     'command.skeleton:command-name',
+        // ]);
     }
 }
